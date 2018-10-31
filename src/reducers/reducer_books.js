54e6e7356141cfec59ce0708f19d75a441878b27
Luan Orlandi <orlandi.luan@gmail.com>
2018-10-31 00:39:02 -0300

    Lecture 41 reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: "JavaScript: The Good Parts" },
+    { title: "Harry Potter" },
+    { title: "The Dark Tower" },
+    { title: "Eloquent Ruby" },
+    { title: "JavaScript: The Good Parts" }
+  ];
+}
