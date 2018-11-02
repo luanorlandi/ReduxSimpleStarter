d42d0ac8b4a10a6d93e348c3853b53f89a873176
Luan Orlandi <orlandi.luan@gmail.com>
2018-11-02 20:10:35 -0300

    Revert to starting project

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
-import reducerWeather from './reducer_weather';
-
 const rootReducer = combineReducers({
-  weather: reducerWeather
+  state: (state = {}) => state
 });
 
 export default rootReducer;
